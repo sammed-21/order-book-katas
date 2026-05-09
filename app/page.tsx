ff4fe89d38bd7432eb2655f2ec00a31606d90d00
sammed-21 <sammed2110@gmail.com>
2026-05-09 12:08:49 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-serif font-semibold">Order Book</h1>
+    </div>
+  );
+}
